15c5c6adabab74e57c9e32f6a468129054e34383
Shu <shul.max9303@gmail.com>
2022-05-30 15:01:32 -0400

    Finished the data fetch part of report function. Trying to design a diagram or table to display the exercise and diet

--- src/components/cards/ReportCard.js
+++ src/components/cards/ReportCard.js
@@ -8,6 +8,10 @@ const ReportCard = (props) => {
 
   const low = new Date(date.getFullYear(), date.getMonth(), date.getDate()-3);
   const high = new Date(date.getFullYear(), date.getMonth(), date.getDate()+3);
+
+  const dateFilter=(item)=>{
+    return new Date(item.date)>=low && new Date(item.date)<=high
+  }
   console.log(low);
   console.log(high);
 
@@ -19,25 +23,25 @@ const ReportCard = (props) => {
     const getExercises = async()=>{
       const q = query(collection(db,"exercises"),where("uid","==",uid));
       const data = await getDocs(q);
-
       const entries = data.docs.map(doc=>({...doc.data()}));
-      const filtered = entries.filter( entry => new Date(entry.date)>=low && new Date(entry.date)<=high)
+      const filtered = entries.filter(dateFilter);
       console.table(filtered);
-      setexerciseReports();
+      setexerciseReports(filtered);
     }
 
     const getDiet = async()=>{
       const q = query(collection(db,"diet"),where("uid","==",uid));
       const data = await getDocs(q);
-
       const entries = data.docs.map(doc=>({...doc.data()}));
-      setDietReports(entries);
+      const filtered = entries.filter(dateFilter);
+      console.table(filtered);
+      setDietReports(filtered);
     }
     getExercises();
     getDiet();
 
     console.log(date);
-  },[]);
+  },[date]);
 
   console.table(exerciseReports);
   console.table(dietReports);
